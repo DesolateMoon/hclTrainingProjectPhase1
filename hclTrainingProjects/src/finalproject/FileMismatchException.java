111f90dc0dce0ce17dd273f7e51fc2082c65233b
DesolateMoon <austinnguyening@gmail.com>
2020-12-11 10:55:03 -0800

    Add files via upload

---
+++ hclTrainingProjects/src/finalproject/FileMismatchException.java
@@ -0,0 +1,8 @@
+package finalproject;
+
+public class FileMismatchException extends Exception{
+
+	public FileMismatchException(String message) {
+		super(message);
+	}
+}
---
+++ hclTrainingProjects/src/finalproject/Interface.java
@@ -0,0 +1,16 @@
+package finalproject;
+
+interface Interface {
+	public void changePath();
+	
+	public void listFile();	
+	
+	public void addFile();
+	
+	public void deleteFile();
+	
+	public void searchFile();
+	
+	public void closeApplication();
+	
+}
